fdd56f2d87aaeaf2a68f4fba957aa80268f686d2
david <david@david-PC>
2013-01-28 19:19:22 +0100

    test

---
+++ src/main/java/com/myfavoriteroutes/models/classtouploadmodel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myfavoriteroutes.models;
+
+/**
+ *
+ * @author david
+ */
+public class classtouploadmodel {
+    
+}
